62ccb75e2eaeacff1d27bdd176a286eab5f93479
michaelperryjnr <michaelperryt97@gmail.com>
2024-07-03 13:37:20 +0000

    Created data storage function

---
+++ src/storage/storeData.js
@@ -0,0 +1,10 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const storeStorageData = async (key, value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem(key, jsonValue);
+  } catch (e) {
+    console.log(e);
+  }
+};
